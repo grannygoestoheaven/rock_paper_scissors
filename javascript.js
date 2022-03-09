65e4381c77b1a23285ba6422e72a840984c25d06
grannygoestoheaven <myganou@hotmail.com>
2022-03-09 19:13:09 +0100

    computerPlay+playRound

---
+++ javascript.js
@@ -0,0 +1,38 @@
+function computerPlay() {
+    let items = ['rock', 'paper', 'scissor'];
+    let choice = items[Math.floor(Math.random() * items.length)];
+    // console.log(`Computer chose ${choice}`);
+    return choice;
+}
+
+function playRound(computerSelection, userSelection) {
+    let cleanUserSelection = userSelection.toLowerCase();
+    let winningMessage = 'You won';
+    let losingMessage = 'You lost';
+
+    if (computerSelection === userSelection) {
+        console.log('Play again');
+        return 0;
+    }
+    else if (computerSelection === 'rock' && userSelection === 'scissor') {
+        return (`${losingMessage} rock beats scissor`);
+    }
+    else if (computerSelection === 'rock' && userSelection === 'paper') {
+        return (`${winningMessage} paper beats rock`);
+    }
+    else if (computerSelection === 'paper' && userSelection === 'rock') {
+        return (`${losingMessage} paper beats rock`);
+    }
+    else if (computerSelection === 'paper' && userSelection === 'scissor') {
+        return (`${winningMessage} scissor beats paper`);
+    }
+    else if (computerSelection === 'scissor' && userSelection === 'paper') {
+        return (`${losingMessage} scissor beats paper`);
+    }
+    else if (computerSelection === 'scissor' && userSelection === 'rock') {
+        return (`${winningMessage} rock beats scissor`);
+    }
+}
+
+const playerChoice = 'rock';
+const computerChoice = computerPlay();
